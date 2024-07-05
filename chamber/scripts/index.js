6d7169d65de38effba1c2f177c27b809317260df
chemcarl08 <chemcarl08@hotmail.com>
2024-07-04 23:59:31 -0600

    update

---
+++ chamber/scripts/index.js
@@ -0,0 +1,15 @@
+document.addEventListener('DOMContentLoaded', function () {
+    document.getElementById('currentyear').textContent = new Date().getFullYear();
+
+    document.getElementById('lastModified').textContent = 'Last Modified: ' + document.lastModified;
+    const menuToggle = document.querySelector('#menuToggle');
+    const menuLinks = document.querySelector('#menuLinks');
+
+    console.log(menuToggle);
+
+    menuToggle.addEventListener('click', function () {
+        console.log('Menu toggle clicked');
+        menuLinks.classList.toggle('active');
+        menuToggle.textContent = menuLinks.classList.contains('active') ? '✖' : '≡';
+    });
+});
